e8d3441c745761693b01e617ef5492d876cbf03b
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-04-11 17:47:27 +0200

    refactoring sideMenu.js

--- 001_manage-my-paroll-rails/app/javascript/custom/sideMenu.js
+++ 001_manage-my-paroll-rails/app/javascript/custom/sideMenu.js
@@ -1,4 +1,4 @@
-// Get all the elements
+// get all the links
 const arrow = document.querySelector("#arrow");
 const bookmark_icon = document.querySelector("#bookmark-icon");
 const close_icon = document.querySelector("#close-icon");
@@ -7,35 +7,46 @@ const menu_icon = document.querySelector(".menu-icon");
 const sideMenu = document.querySelector(".sidebar");
 const subMenu = document.querySelector("#submenu");
 
-// Function to toggle SidebarMenu
-function toggleSidebarMenu() {
+// Function to open SidebarMenu
+function openSidebarMenu() {
   sideMenu.classList.toggle("translate-x-[-100%]");
-  toggleFixedMenu();
+  console.log("Test to open modal");
 }
 
 // Function to close SidebarMenu
 function closeSidebarMenu() {
-  toggleSidebarMenu();
+  sideMenu.classList.toggle("translate-x-[-100%]");
   main_container.style.marginLeft = "";
   bookmark_icon.style.fill = "none";
+  console.log("Test to close modal");
 }
-
 // Function dropdown for subMenu
 function dropdown() {
   subMenu.classList.toggle("hidden");
   arrow.classList.toggle("rotate-180");
+  console.log("Test to dropdown menu");
 }
 
-// Function to toggle fixed menu
+// Function toggle fixed menu
 function toggleFixedMenu() {
-  const isOpen = !sideMenu.classList.contains("translate-x-[-100%]");
-  main_container.classList.toggle("ml-0", isOpen);
-  main_container.style.marginLeft = isOpen ? "300px" : "";
-  bookmark_icon.style.fill = isOpen ? "red" : "none";
+  if (!sideMenu.classList.contains("translate-x-[-100%]")) {
+    main_container.classList.toggle("ml-0");
+    main_container.style.marginLeft = "300px";
+    bookmark_icon.style.fill = "red";
+    console.log("Test for toggleFixedMenu");
+  } else {
+    main_container.classList.toggle("ml-0");
+    main_container.style.marginLeft = "";
+  }
 }
 
-// Event listeners
-menu_icon.addEventListener("click", toggleSidebarMenu);
+// Open menu when click on menu icon
+menu_icon.addEventListener("click", () => {
+  sideMenu.classList.contains("translate-x-[-100%]")
+    ? openSidebarMenu()
+    : closeSidebarMenu();
+});
+
 close_icon.addEventListener("click", closeSidebarMenu);
 bookmark_icon.addEventListener("click", toggleFixedMenu);
 arrow.addEventListener("click", dropdown);
