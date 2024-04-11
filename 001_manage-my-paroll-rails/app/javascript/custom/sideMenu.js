15442c84f9100fca835d62da591e793a5cc2bd25
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-04-11 17:21:15 +0200

    refactoring sideMenu.js

--- 001_manage-my-paroll-rails/app/javascript/custom/sideMenu.js
+++ 001_manage-my-paroll-rails/app/javascript/custom/sideMenu.js
@@ -1,56 +1,41 @@
-// get all the links
+// Get all the elements
 const arrow = document.querySelector("#arrow");
-const bookmark_icon = document.querySelector('#bookmark-icon');
-const close_icon = document.querySelector('#close-icon');
+const bookmark_icon = document.querySelector("#bookmark-icon");
+const close_icon = document.querySelector("#close-icon");
 const main_container = document.querySelector(".main-container");
-const menu_icon = document.querySelector('.menu-icon');
-const sideMenu = document.querySelector('.sidebar');
+const menu_icon = document.querySelector(".menu-icon");
+const sideMenu = document.querySelector(".sidebar");
 const subMenu = document.querySelector("#submenu");
 
-
-// Function to open SidebarMenu
-function openSidebarMenu() {
-  sideMenu.classList.toggle('translate-x-[-100%]');
-  console.log('Test to open modal')
+// Function to toggle SidebarMenu
+function toggleSidebarMenu() {
+  sideMenu.classList.toggle("translate-x-[-100%]");
+  toggleFixedMenu();
 }
 
 // Function to close SidebarMenu
 function closeSidebarMenu() {
-  sideMenu.classList.toggle('translate-x-[-100%]');
-  main_container.style.marginLeft = '';
-  bookmark_icon.style.fill = 'none';
-  console.log('Test to close modal')
+  toggleSidebarMenu();
+  main_container.style.marginLeft = "";
+  bookmark_icon.style.fill = "none";
 }
+
 // Function dropdown for subMenu
 function dropdown() {
   subMenu.classList.toggle("hidden");
   arrow.classList.toggle("rotate-180");
-  console.log('Test to dropdown menu')
 }
 
-// Function toggle fixed menu
+// Function to toggle fixed menu
 function toggleFixedMenu() {
-  if (!sideMenu.classList.contains('translate-x-[-100%]')) {
-    main_container.classList.toggle('ml-0');
-    main_container.style.marginLeft = "300px";
-    bookmark_icon.style.fill = 'red';
-    console.log('Test for toggleFixedMenu');
-
-  } else {
-    main_container.classList.toggle('ml-0');
-    main_container.style.marginLeft = '';
-  }
+  const isOpen = !sideMenu.classList.contains("translate-x-[-100%]");
+  main_container.classList.toggle("ml-0", isOpen);
+  main_container.style.marginLeft = isOpen ? "300px" : "";
+  bookmark_icon.style.fill = isOpen ? "red" : "none";
 }
-// Open menu when click on menu icon
-menu_icon.addEventListener('click', () => {
-  sideMenu.classList.contains('translate-x-[-100%]')? openSidebarMenu(): closeSidebarMenu();
-  console.log('Test menu icon');
-});
-// Close menu when click on close icon
-close_icon.addEventListener('click', closeSidebarMenu);
 
-// Toggle fixed menu on bookmark icon
-bookmark_icon.addEventListener('click', toggleFixedMenu);
-
-// Toggle fixed menu on bookmark icon
-arrow.addEventListener('click', dropdown);
+// Event listeners
+menu_icon.addEventListener("click", toggleSidebarMenu);
+close_icon.addEventListener("click", closeSidebarMenu);
+bookmark_icon.addEventListener("click", toggleFixedMenu);
+arrow.addEventListener("click", dropdown);
